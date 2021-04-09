673d40ee7a0a631b65e3e339c746f62c6cc1d1c1
Karel J. van der Lelij <kadmosb1@users.noreply.github.com>
2021-04-09 08:10:47 +0200

    Eerste taak uitgevoerd

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println ("Ik heb taak 1 uitgevoerd");
+        System.out.println ("Ik heb taak 2 uitgevoerd");
     }
 }
